b9ddc1fce33c9dbefe59204f8ee46713e1877027
Rachmawan Atmaji <themusketeer17@gmail.com>
2025-01-20 09:36:29 +0700

    update to countries API

--- src/components/TestMultiSelect.vue
+++ src/components/TestMultiSelect.vue
@@ -5,21 +5,22 @@
     <div>
       <multiselect
         v-model="value"
-        placeholder="city name?"
-        label="city"
-        track-by="city_ascii"
+        placeholder="country name?"
+        label="country_name"
+        track-by="country_code"
         :options="options"
         :multiple="true"
         :taggable="true"
       ></multiselect>
     </div>
 
-    <pre class="language-json"><code>{{ value  }}</code></pre>
+    <pre class="language-json"><code>{{ value }}</code></pre>
   </div>
 </template>
 
 <script>
 import Multiselect from "vue-multiselect";
+import axios from "axios";
 
 export default {
   components: {
@@ -28,45 +29,51 @@ export default {
   data() {
     return {
       value: null,
-      options: [
-        {
-          city: "San Martin",
-          city_ascii: "San Martin",
-          lat: -33.06998533,
-          lng: -68.49001612,
-          pop: 99974,
-          country: "Argentina",
-          iso2: "AR",
-          iso3: "ARG",
-          province: "Mendoza",
-          timezone: "America/Argentina/Mendoza",
-        },
-        {
-          city: "San Nicolas",
-          city_ascii: "San Nicolas",
-          lat: -33.33002114,
-          lng: -60.24000289,
-          pop: 117123.5,
-          country: "Argentina",
-          iso2: "AR",
-          iso3: "ARG",
-          province: "Ciudad de Buenos Aires",
-          timezone: "America/Argentina/Buenos_Aires",
-        },
-        {
-          city: "San Francisco",
-          city_ascii: "San Francisco",
-          lat: -31.43003375,
-          lng: -62.08996749,
-          pop: 43231,
-          country: "Argentina",
-          iso2: "AR",
-          iso3: "ARG",
-          province: "Córdoba",
-          timezone: "America/Argentina/Cordoba",
-        },
-      ],
+      options: [],
+      // options: [
+      //   {
+      //     city: "San Martin",
+      //     city_ascii: "San Martin",
+      //     lat: -33.06998533,
+      //     lng: -68.49001612,
+      //     pop: 99974,
+      //     country: "Argentina",
+      //     iso2: "AR",
+      //     iso3: "ARG",
+      //     province: "Mendoza",
+      //     timezone: "America/Argentina/Mendoza",
+      //   },
+      //   {
+      //     city: "San Nicolas",
+      //     city_ascii: "San Nicolas",
+      //     lat: -33.33002114,
+      //     lng: -60.24000289,
+      //     pop: 117123.5,
+      //     country: "Argentina",
+      //     iso2: "AR",
+      //     iso3: "ARG",
+      //     province: "Ciudad de Buenos Aires",
+      //     timezone: "America/Argentina/Buenos_Aires",
+      //   },
+      //   {
+      //     city: "San Francisco",
+      //     city_ascii: "San Francisco",
+      //     lat: -31.43003375,
+      //     lng: -62.08996749,
+      //     pop: 43231,
+      //     country: "Argentina",
+      //     iso2: "AR",
+      //     iso3: "ARG",
+      //     province: "Córdoba",
+      //     timezone: "America/Argentina/Cordoba",
+      //   },
+      // ],
     };
+  },
+  mounted() {
+    axios.get("http://localhost:9000/api/countries/all").then((response) => {
+      this.options = response.data;
+    });
   },
 };
 </script>

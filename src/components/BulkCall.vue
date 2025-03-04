54f0517ab000f1ecf351c825156a583b46f50ace
Rachmawan Atmaji <themusketeer17@gmail.com>
2025-03-04 15:34:02 +0700

    test bulk API call

--- src/components/BulkCall.vue
+++ src/components/BulkCall.vue
@@ -4,26 +4,71 @@
   <fwb-button gradient="green-blue" size="md" @click="increaseProgress"
     >Increase Progress</fwb-button
   >
+
+  <fwb-button gradient="green-blue" size="md" @click="submitPostDynamic"
+    >Submit Post Dynamic</fwb-button
+  >
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, toHandlers } from "vue";
 import axios from "axios";
 import { FwbProgress, FwbButton } from "flowbite-vue";
 </script>
 
 <script>
-const pg = ref(50);
-
 export default {
   data() {
     return {
-      progVal: 50,
+      progVal: 0,
+      reqList: [
+        { title: "Alice", body: "HHH", userId: 1 },
+        { title: "Bob", body: "III", userId: 2 },
+        { title: "Charles", body: "JJJ", userId: 3 },
+      ],
     };
   },
   methods: {
     increaseProgress() {
       this.progVal += 10;
+    },
+    submitPostDynamic() {
+      const requests = [
+        axios.post("https://jsonplaceholder.typicode.com/posts", {
+          title: "Alice",
+          body: "HHH",
+          userId: 1,
+        }),
+        axios.post("https://jsonplaceholder.typicode.com/posts", {
+          title: "Bob",
+          body: "III",
+          userId: 2,
+        }),
+        axios.post("https://jsonplaceholder.typicode.com/posts", {
+          title: "Charles",
+          body: "JJJ",
+          userId: 3,
+        }),
+        axios.post("https://jsonplaceholder.typicode.com/posts", {
+          title: "Donna",
+          body: "KKK",
+          userId: 4,
+        }),
+      ];
+
+      axios
+        .all(requests)
+        .then(
+          axios.spread((...responses) => {
+            responses.forEach((response, index) => {
+              console.log(`Data from request ${index + 1}:`, response.data);
+              this.progVal += 25;
+            });
+          })
+        )
+        .catch((errors) => {
+          console.error("Error fetching data:", errors);
+        });
     },
   },
 };
